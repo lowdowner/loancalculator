51bd387405bda151ca83df3ffa2b6f1cdc33ef82
lowdowner <96691405+lowdowner@users.noreply.github.com>
2022-12-08 22:19:42 +0000

    Update URLs

--- loancalculator/views.py
+++ loancalculator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import InvestmentForm
 
-# Create your views here.
+
+class Index(View):
+    def get(self, request):
+        form = InvestmentForm()
+        return render(request, 'loancalculator/index.html', {'form': form})
+
+    def post(self, request):
+        pass
