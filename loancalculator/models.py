08b7e47c97dc7ad9c906fb1a1107575ba1c0f064
lowdowner <96691405+lowdowner@users.noreply.github.com>
2022-12-24 22:08:58 +0000

    Results on homepage

--- loancalculator/models.py
+++ loancalculator/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+ 
+class InvestmentForm(models.Model):
+    starting_amount = models.FloatField()
+    number_of_years = models.FloatField()
+    return_rate = models.FloatField()
+    annual_additional_contribution = models.FloatField()
 
 # Create your models here.
--- loancalculator/views.py
+++ loancalculator/views.py
@@ -5,38 +5,75 @@ from .forms import InvestmentForm
 
 class Index(View):
     def get(self, request):
+
+        '''
         form = InvestmentForm()
+        return render(request, 'loancalculator/index.html', {'form': form})
+        '''
+        # Fill in the initial values on the unbound form:
+        initial = {
+            'starting_amount': request.session.get('starting_amount'),
+            'number_of_years': request.session.get('number_of_years'),
+            'return_rate': request.session.get('return_rate'),
+            'annual_additional_contribution': request.session.get('annual_additional_contribution'),
+        }
+
+        form = InvestmentForm(initial=initial)
         return render(request, 'loancalculator/index.html', {'form': form})
 
     def post(self, request):
         form = InvestmentForm(request.POST)
 
         if form.is_valid():
-            total_result = form.cleaned_data['starting_amount'] 
-            total_interest = 0 
-            yearly_results = {}
+                
+                # After getting the cleaned_data, set a session variable to hold it
+                total_result = form.cleaned_data['starting_amount']
+                request.session['starting_amount'] = total_result
 
-            for i in range(1, int(form.cleaned_data['number_of_years'] +1)):
-                yearly_results[i] = {}
+                number_of_years = int(form.cleaned_data['number_of_years'])
+                request.session['number_of_years'] = number_of_years
 
-                # CALCULATE THE INTEREST 
-                interest = total_result * (form.cleaned_data['return_rate'] / 100)
-                total_result += interest 
-                total_interest += interest
+                return_rate = form.cleaned_data['return_rate']
+                request.session['return_rate'] = return_rate
 
-                # ADDITIONAL CONTRIBUTION 
-                total_result += form.cleaned_data['annual_additional_contribution']
+                annual_additional_contribution = form.cleaned_data['annual_additional_contribution']
+                request.session['annual_additional_contribution'] = annual_additional_contribution
+                total_interest = 0
+                yearly_results = {}
 
-                # SET YEARLY RESULTS 
-                yearly_results[i]['interest']  = round(total_interest, 2)
-                yearly_results[i]['total'] = round(total_result,2)
+                for i in range(1, int(form.cleaned_data['number_of_years'] +1)):
+                    yearly_results[i] = {}
 
-                # CREATE CONTEXT 
-                context = {
-                    'total_result': round(total_result,2),
-                    'yearly_results': yearly_results,
-                    'number_of_years': int(form.cleaned_data['number_of_years'])
-                }
+                    # CALCULATE THE INTEREST 
+                    interest = total_result * (form.cleaned_data['return_rate'] / 100)
+                    total_result += interest 
+                    total_interest += interest
 
-            # RENDER THE TEMPLATE
-        return render(request, 'loancalculator/result.html', context)
+                    # ADDITIONAL CONTRIBUTION 
+                    total_result += form.cleaned_data['annual_additional_contribution']
+
+                    # SET YEARLY RESULTS 
+                    yearly_results[i]['interest']  = round(total_interest, 2)
+                    yearly_results[i]['total'] = round(total_result,2)
+
+
+                    # CREATE CONTEXT 
+                    context = {
+                        'form':form,
+                        'total_result': round(total_result,2),
+                        'yearly_results': yearly_results,
+                        'number_of_years': int(form.cleaned_data['number_of_years'])
+                    }
+            
+                # RENDER THE TEMPLATE
+
+                return render(request, 'loancalculator/index.html', context)
+
+
+        
+        else:
+            form = InvestmentForm()
+            return render(request, 'loancalculator/index.html', {'form':form})
+        
+            
+        
